47c9f6da6580d196da11f48c446d8bccdb686c68
MostakemHossain <mostakememon123@gmail.com>
2024-01-25 21:06:51 +0600

    add writter and admin index added

--- client/src/dashboard/layout/MainLayout.jsx
+++ client/src/dashboard/layout/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <div className="min-w-screen bg-gray-100">
       <SideBar />
-      <div className="ml-[250px] w-[calc(100vh - 250px)] min-h-[vh]">
+      <div className="ml-[250px] w-[calc(100vh - 260px)] min-h-[100vh]">
         <Header />
         <div className="p-4">
           <div className="pt-[85px]">
